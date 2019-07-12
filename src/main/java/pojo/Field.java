74f63c83d0d5394a56c0993085a614a0d798cf99
hola <holakt@qq.com>
2019-07-12 11:45:41 +0800

    fork然后修改文件

---
+++ src/main/java/pojo/Field.java
@@ -0,0 +1,34 @@
+package pojo;
+
+/**
+ * Created by li on 2017/12/18.
+ */
+public class Field {
+    public String name;
+    public String type;
+    public String names;
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
